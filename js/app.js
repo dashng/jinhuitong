b401369b7af59b2002e965cca2375076b52cd91e
Dai Shaoshuai <shdai@cisco.com>
2015-07-31 09:57:32 +0800

    'v2'

--- js/app.js
+++ js/app.js
@@ -1,19 +1,55 @@
 $(function(){
+	var $body = $('body');
 	var menu_list = $('.nav1').find('a');
+	var to_top_hander = $('.to-top');
 	menu_list.each(function(i, $div){
 		$(this).on('click', function(){
-			$('.index-panel, .panel-nav:not(".nav1")').hide();
-			var index = i*1+2;
-			$('.index-panel-'+index+', '+ '.nav'+index).show();
+			var index = i+2;
+			$body.animate({
+				scrollTop: $('.index-panel-'+index).offset().top+'px'
+			},
+			500,
+			function(){
+					
+			});
 		});
 	});
-	menu_list.filter(':first').trigger('click');
-	
-	
+	$(window).scroll(function(){
+		var $top = $(this).scrollTop();
+		if($(window).width() <= 1200){
+			to_top_hander
+				.hide();
+		}else if($top >= $('.index-panel-2').offset().top){
+			to_top_hander
+				.show()
+				.css('top', $top + $(window).height() - 90);
+		}else{
+			to_top_hander
+				.hide();
+		}
+	});
+	$(window).trigger('scroll');
 	$('div.nav-content > div.nav li').on('click', function(event){
 		event.stopPropagation();
 		var href = $(this).attr('data-href');
 		window.location.href = href;
 	});
-	
+	$('.to-top').on('click', function(){
+		$(window).scrollTop(0);
+	});
+	$('.redian').hover(function(event){
+		event.stopPropagation();
+		$('.redian-content')
+		.animate({
+			opacity: 1.0
+		}, 500, function() {
+			
+		});
+	}, function(event){
+		event.stopPropagation();
+		$('.redian-content').animate({
+			opacity: 0.0
+		}, 500, function() {
+		});
+	});
 });
